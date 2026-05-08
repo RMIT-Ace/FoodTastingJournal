d684e56903a322408393cdbc0cd3ac4af79e9738
RMIT-Ace <ace.authors@rmit.edu.au>
2026-05-08 10:56:06 +1000

    Initial Xcode project

---
+++ FoodFlicks/FoodFlicks/FoodFlicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFlicksApp.swift
+//  FoodFlicks
+//
+//  Created by Ace on 8/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoodFlicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
