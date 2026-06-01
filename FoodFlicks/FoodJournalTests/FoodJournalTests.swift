4a26947ea01d39489dfd026392c32086a5b46034
RMIT-Ace <ace.authors@rmit.edu.au>
2026-06-01 16:11:25 +1000

    init TDD test cases

---
+++ FoodFlicks/FoodJournalTests/FoodJournalTests.swift
@@ -0,0 +1,23 @@
+//
+//  FoodJournalTests.swift
+//  FoodJournalTests
+//
+//  Created by Ace on 1/6/2026.
+//
+
+import Testing
+
+struct FoodJournalTests {
+
+    /// All recorded restaurants within 2 kilometers of a given location
+    /// listed.
+    @Test("Nearby restaurants exist")
+    func nearByRestaurants() throws {
+        Issue.record("To be implemented")
+    }
+    
+    @Test("All recorded restaurants calculated with distances from a given location.")
+    func allRestaurantsWithDistances() throws {
+        Issue.record("To be implemented")
+    }
+}
