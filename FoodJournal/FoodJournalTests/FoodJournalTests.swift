bdd311417a9a3a25dd8e3c9e3a5be54c2306e4ad
RMIT-Ace <ace.authors@rmit.edu.au>
2026-06-17 16:29:08 +1000

    Setup always-fail test cases

--- FoodJournal/FoodJournalTests/FoodJournalTests.swift
+++ FoodJournal/FoodJournalTests/FoodJournalTests.swift
@@ -13,55 +13,17 @@ import CoreLocation
 
 struct FoodJournalTests {
     
-    static let melbLocation = CLLocation(latitude: -37.8136, longitude: 144.9631)
-    
-    static let nearRestaurants: [Restaurant] = [
-        Restaurant(name: "Restaurant 1",
-            location: .init( latitude: -37.8136, longitude: 144.9632 )
-        ),
-        Restaurant(name: "Restaurant 2",
-            location: .init( latitude: -37.8137, longitude: 144.9633 )
-        ),
-    ]
-    
-    static let farRestaurants: [Restaurant] = [
-        Restaurant(name: "Restaurant 3",
-            location: .init( latitude: -37.8136, longitude: 144.9742 )
-        ),
-        Restaurant(name: "Restaurant 4",
-            location: .init( latitude: -37.8137, longitude: 144.9743 )
-        ),
-    ]
-    
-
-    static let visitedRestaurants: [Restaurant] = {
-        nearRestaurants + farRestaurants
-    }()
-    
-    let locationManager: LocationManager = {
-        let manager = LocationManager()
-        manager.currentLocation = Self.melbLocation
-        manager.visitedRestaurants = visitedRestaurants
-        return manager
-    }()
-    
     // MARK: - Test Cases -
     
-    @Test("Location manager provides location")
+    static let alwaysFailed: Bool = 1 == 2
+    
+    @Test("Location manager provides current location")
     func getCurrentLocation() async throws {
-        #expect(locationManager.currentLocation == Self.melbLocation)
+        Issue.record("No test implemented")
     }
     
     @Test("Nearby restaurants are within the nearby distance.")
     func nearByRestaurantsWithinDistance() throws {
-        guard let currentLocation = locationManager.currentLocation else {
-            #warning("No location")
-            return
-        }
-        for restaurant in locationManager.nearByRestaurants {
-            print(restaurant.name, restaurant.location.distance(from: currentLocation))
-            let restaurantDistace = restaurant.location.distance(from: currentLocation)
-            #expect(restaurantDistace <= locationManager.nearByDistance)
-        }
+        #expect(Self.alwaysFailed, "To be implemented")
     }
 }
