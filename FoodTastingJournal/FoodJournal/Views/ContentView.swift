189ffa8001db039c31ae10e60f90cb562ff50223
RMIT-Ace <ace.authors@rmit.edu.au>
2026-07-06 09:46:13 +1000

    Make 2nd iteration tdd passes

--- FoodTastingJournal/FoodJournal/Views/ContentView.swift
+++ FoodTastingJournal/FoodJournal/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         TabView {
             NearbyView()
+                .accessibilityIdentifier("NearbyView")
                 .tabItem {
                     Label("Nearby", systemImage: "location")
                 }
